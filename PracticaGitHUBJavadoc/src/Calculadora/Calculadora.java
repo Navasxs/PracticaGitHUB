88fa37a4edb454a6d2609ec834b94a370388c156
alumno <alumno@MintEntornos>
2018-03-04 22:53:47 +0100

    Actualizacion de la clase Calculadora nº2
    Se ha añadido la documentacion javadoc

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -1,24 +1,45 @@
 package Calculadora;
 
+/**
+ * @author Jesus Navascues
+ * @version 1.0
+ * 
+ */
+
 public class Calculadora {
 
+	
 	double op1;
 	double op2;
 	
 	
+	/**
+	 * @param op1 primer operando de la calculadora
+	 * @param op2 segundo operando de la calculadora
+	 */
 	public Calculadora(double op1, double op2) {
 		this.op1 = op1;
 		this.op2 = op2;
 	}
 	
+	/**
+	 * @return devuelve la suma de op1 y op2
+	 */
 	double suma() {
 		return op1+op2;
 	}
 	
+	/**
+	 * @return devuelve la resta de op1 y op2
+	 */
 	double resta() {
 		return op1-op2;
 	}
 	
+	/**
+	 * @return devuelve la division de op1 y op2
+	 * @throws IllegalArgumentException ningun numero puede ser dividido por 0
+	 */
 	double division() {
 		if(op2==0) {
 			throw new IllegalArgumentException("El divisor es 0");
@@ -32,6 +53,9 @@ public class Calculadora {
 	//-Un n�mero "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
 	//As� 232^0 = 1; y 2^(-3)=1/2^3=1/8
 	
+	/**
+	 * @return devuelve op1 elevado a op2
+	 */
 	double potencia() {
 		if(op2==0) {
 			return 1;
