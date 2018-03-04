6bec3fbb83418c8244fce60967145167a2ab5300
alumno <alumno@MintEntornos>
2018-03-04 22:28:18 +0100

    Actualizacion de la clase Calculadora
    Se ha añadido a la clase Calculadora la funcion Potencia asi como el
    metodo toString, además se incluye un programa de pruebas q verifica el
    funcionamiento de todas las funciones de Calculadora

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -26,17 +26,36 @@ public class Calculadora {
 		return op1/op2;
 	}
 	
-	//TODO: Añadir la operación potencia, que devuelve
+	//TODO: Aï¿½adir la operaciï¿½n potencia, que devuelve
 	//op1 elevado a op2 con las siguientes reglas: 
-	//-Por convenio cualquier número elevado a 0 es igual a 1.
-	//-Un número "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
-	//Así 232^0 = 1; y 2^(-3)=1/2^3=1/8
+	//-Por convenio cualquier nï¿½mero elevado a 0 es igual a 1.
+	//-Un nï¿½mero "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
+	//Asï¿½ 232^0 = 1; y 2^(-3)=1/2^3=1/8
 	
+	double potencia() {
+		if(op2==0) {
+			return 1;
+		}
+		if(op2<0) {
+			return (1/(Math.pow(op1,op2)));
+		}
+		else {
+			return Math.pow(op1,op2);
+		}
+	}
+
+		
 	
-	//TODO: Añadir la operacion toString que devuelva los operandos
+	//TODO: Aï¿½adir la operacion toString que devuelva los operandos
 	//la cadena:
 	//"Calculadora de <vuestroNombre>: "
 	//"operando1 = <op1>"
 	//"operando2 = <op2>"
+	
+
+	@Override
+	public String toString() {
+		return "Calculadora de JesusNavascues [op1=" + op1 + ", op2=" + op2 + "]";
+	}
 	
 }
--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -5,7 +5,24 @@ public class Programa {
 	//TODO: Crear un programa principal que 
 	//utilice todas las funciones de la calculadora
 	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
+	//que habrï¿½is aï¿½adido.
 	//Ejecutadlo.
-
+	
+	public static void main(String[] args) {
+		Calculadora c1= new Calculadora(6,3);
+		Calculadora c2= new Calculadora(6,0);
+		Calculadora c3= new Calculadora(6,-3);
+		
+		System.out.println(c1);
+		System.out.println("suma de los operandos:"+ c1.suma());
+		System.out.println("resta de los operandos:"+ c1.resta());
+		System.out.println("division de los operandos:"+ c1.division());
+		System.out.println("potencia de los operandos:" + c1.potencia());
+		System.out.println(c3+ "comprobacion de la potencia negativa:");
+		System.out.println("potencia de los operandos:" + c3.potencia());
+		System.out.println(c2 + "comprobacion de la division y la potencia entre 0:");
+		System.out.println("potencia de los operandos:" + c2.potencia());
+		System.out.println("division de los operandos:"+ c2.division());
+		
+	}
 }
